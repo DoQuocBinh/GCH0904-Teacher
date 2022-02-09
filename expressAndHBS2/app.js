352834bf97d379d94d85f04897ee8f171b9b57d4
DoQuocBinh <binhdq@fpt.edu.vn>
2022-02-09 09:14:23 +0700

    ok

---
+++ expressAndHBS2/app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.set('view engine','hbs')
+//khai bao thu muc public noi chua static files: images, stylesheet,..
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.post('/register',(req,res)=>{
+    //lay data user nhap tu form Register
+    const name = req.body.txtName
+    const clazz = req.body.txtClass
+    //xay dung doi tuong user
+    const user = {
+        name : name,
+        clazz: clazz
+    }
+    fs.appendFileSync('mydata.txt',JSON.stringify(user))
+    //tao variable user trong template done
+    res.render("done",{user:user})
+
+})
+
+app.get('/register',(req,res)=>{
+    res.render('dangky')
+})
+
+app.get('/',(req,res)=>{
+    const now = new Date()
+    //tao mot variable 'ngay' trong template index, gia tri bang bien 'now'
+    res.render('index',{ngay:now})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+
+
