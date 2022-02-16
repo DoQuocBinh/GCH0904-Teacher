ebe1645a93950874011f916748a068999d9cc673
DoQuocBinh <binhdq@fpt.edu.vn>
2022-02-16 09:02:25 +0700

    insert into db

--- expressAndHBS2/app.js
+++ expressAndHBS2/app.js
@@ -1,20 +1,26 @@
 const express = require('express')
-const fs = require('fs')
+const fs = require('fs');
+const async = require('hbs/lib/async');
 const app = express()
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://tommy:123456abc@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/NoSQLBoosterSamples?authSource=admin&replicaSet=Cluster0-shard-0&ssl=true';
+
 
 app.set('view engine','hbs')
 //khai bao thu muc public noi chua static files: images, stylesheet,..
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
-app.post('/register',(req,res)=>{
+app.post('/register', async (req,res)=>{
     //lay data user nhap tu form Register
     const name = req.body.txtName
     const clazz = req.body.txtClass
+    const phone = req.body.txtPhone
     //xay dung doi tuong user
     const user = {
         name : name,
-        clazz: clazz
+        clazz: clazz,
+        phone: phone
     }
 
     //kiem tra(input validation)
@@ -29,7 +35,10 @@ app.post('/register',(req,res)=>{
         return
     }
     
-    fs.appendFileSync('mydata.txt',JSON.stringify(user))
+    //fs.appendFileSync('mydata.txt',JSON.stringify(user))
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("NoSQLBoosterSamples");//NoSQLBoosterSamples: ten database
+    await dbo.collection("SanPham").insertOne(user) //SanPham: ten bang
     //tao variable user trong template done
     res.render("done",{user:user})
 
